c75e95155864ce981d886b9e9bbf8f3fa5c2f43e
Denis Martins <martins.denis@gmail.com>
2024-07-01 14:16:10 -0300

    feat: adding routes

--- lib/app/routes/route_app.dart
+++ lib/app/routes/route_app.dart
@@ -3,7 +3,6 @@ import 'package:go_router/go_router.dart';
 import 'package:start_app_structure/app/constants/routes_constants.dart';
 import 'package:start_app_structure/app/pages/home/home_page.dart';
 import 'package:start_app_structure/app/pages/layout/layout_app.dart';
-import 'package:start_app_structure/app/pages/settings/settings_page.dart';
 
 class RouteApp {
   static final _rootNavigatorKey = GlobalKey<NavigatorState>();
