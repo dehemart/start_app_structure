9fb90583a328e5dbdcaa282df8e09a765808af53
Denis Martins <martins.denis@gmail.com>
2024-07-01 17:33:56 -0300

    feat: configure use of the routes

--- lib/app/main_app.dart
+++ lib/app/main_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:start_app_structure/app/constants/app_constants.dart';
+import 'package:start_app_structure/app/routes/route_app.dart';
 import 'package:start_app_structure/app/themes/theme_app.dart';
 
 class MainApp extends StatelessWidget {
@@ -7,12 +8,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: AppConstants.appTitle,
       themeMode: ThemeMode.dark,
       theme: ThemeApp.lightThemeData,
       darkTheme: ThemeApp.darkThemeData,
+      routerConfig: RouteApp.router,
     );
   }
 }
